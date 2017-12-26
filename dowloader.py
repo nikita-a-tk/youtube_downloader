7be4146dfaba70d8af054f25f40bbe0bb2eb12ad
Nikita Tkachev <nikita-a-tk@yandex.ru>
2017-12-26 22:27:21 +0800

    Implemented downloading function

--- dowloader.py
+++ dowloader.py
@@ -1,8 +1,17 @@
 from pytube import YouTube
 print("Welcome to YouTube Downloader app!")
 #video_url = input("Which video you want to download?\n")
-video_url = "https://www.youtube.com/watch?v=M0Ypzm7pVw4"
-videos = YouTube(video_url).streams.all()
+video_url = "https://www.youtube.com/watch?v=M0Ypzm7pVw4" # short video for testing
+video = YouTube(video_url)
+print(video.title)
+streams = video.streams.all()
+i = 1
+for stream in streams:
+	print(str(i) + ": " + str(stream))
+	i += 1
 
-for v in videos:
-	print(v)
+choice = int(input("Choose video stream: "))
+
+streams[choice - 1].download()
+
+print("{} successfully downloaded!".format(streams[choice - 1].default_filename))
